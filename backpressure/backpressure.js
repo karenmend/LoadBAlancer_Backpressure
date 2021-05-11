725daae95767be973f2e8a23cd9aea979a25b69d
Karen Mendivil <karenlili14@gmail.com>
2021-05-11 15:55:02 -0700

    listo

---
+++ backpressure/backpressure.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const server = http.createServer(
+	(req, res) => {
+		console.log("Concexiones: ", server._connections);
+		setTimeout(
+			() => {
+			 	res.end("OK");
+			}, 10000
+		);
+	}
+);
+
+server.maxConnections = 2;
+server.listen(3020, "localhost");
